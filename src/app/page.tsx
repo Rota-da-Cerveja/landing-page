6fd8dadab5410875384ad4dbe541dacd64124244
Silvio Cavalcanti <silviocavalcanti2011@gmail.com>
2024-04-19 11:08:09 -0300

    feat: adicionado a responsividade para telas md na parte principal da tela

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,17 @@ export default function Home() {
   return (
     <>
       <main
-        className="main flex bg-black bg-cover bg-right-bottom justify-between h-screen w-full bg-no-repeat text-white"
+        className="main flex bg-black bg-cover bg-right-bottom justify-center h-screen w-full bg-no-repeat text-white"
         style={{ backgroundImage: "url('" + background.src + "')" }}>
-          <div className="container m-4 mt-14">
-            <h2 className="text-xl mb-4">
+          <div className="container m-4 md:mt-20 mt-14">
+            <h2 className="text-xl mb-4 md:mb-16 md:text-3xl">
               As melhores cervejas artesanais da sua região?
             </h2>
-            <Image src={logo} alt="Logo" className="w-60"></Image>
-            <h3 className="w-64 text-lg">
+            <Image src={logo} alt="Logo" className="w-60 mb-4 md:mb-16 md:w-[60%]"></Image>
+            <h3 className="w-64 text-base md:text-2xl md:w-[65%] font-bold">
               O aplicativo desenvolvido para você ter a melhor experiência cervejeira possível.
             </h3>
-            <button className="bg-primary-700 py-4 px-7 mt-10 rounded">Quero Conhecer a Rota</button>
+            <button className="bg-primary-700 py-4 md:py-8 px-7 md:px-14 mt-10 text-base md:text-3xl rounded">Quero Conhecer a Rota</button>
           </div>
         {/* <div className="w-1/2 bg-red-500 flex justify-center items-center">
           <div className="flex flex-col gap-[20px]">

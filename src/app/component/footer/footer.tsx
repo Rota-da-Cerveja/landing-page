d04373a3d16a8aa3a47089b25aff958ab0593cd8
Silvio Cavalcanti <silviocavalcanti2011@gmail.com>
2024-04-20 22:00:11 -0300

    feat: criação do arquivo css do footer e modificações da responsividade

--- src/app/component/footer/footer.tsx
+++ src/app/component/footer/footer.tsx
@@ -6,6 +6,7 @@ import { Social } from "../button/social/Social";
 import path from "path";
 import fs from 'fs';
 import { icons } from "../assets/icon";
+import './footer.css';
 
 const inter = Inter({subsets: ["latin"]});
 
@@ -16,36 +17,34 @@ export function Footer() {
 
     const currentYear = new Date().getFullYear();
 
-  return (<footer className="bg-black text-white flex justify-center shadow-footer relative">
-    <section className="container p-5 grid grid-cols-4 grid-rows-6 gap-x-2">
-          <Image src={logo} alt="background" className="w-[80px] self-center col-start-1 justify-self-center"></Image>
-        <div className="grid grid-cols-2 w-full row-start-2 justify-self-center col-span-4 col-start-1 uppercase font-black gap-y-1 gap-x-3">
-              <h1 className={inter.className+" col-span-2 justify-self-center w-max"}>em breve</h1>
-            <StoreButton title={"App Store"} icon={icons.appStore}></StoreButton>
-            <StoreButton title={"Google Play"} icon={icons.playStore}></StoreButton>
+  return (<footer className="footer-wrapper">
+    <section className="footer-container">
+          <Image src={logo} alt="background" className="footer-logo"></Image>
+        <div className="footer-content">
+              <h1 className={`footer-heading ${inter.className}`}>em breve</h1>
+            <StoreButton title="App Store" icon={icons.appStore}></StoreButton>
+            <StoreButton title="Google Play" icon={icons.playStore}></StoreButton>
         </div>
-        <div className="uppercase pt-5 text-sm font-black flex col-start-1 row-span-3 col-span-2 items-self-center row-start-3 flex-col gap-2">
+        <div className="footer-links">
           <a>Sobre</a>    
           <a>Suporte</a>
           <a>Fornecedores</a>
         </div>
 
-        <div className="col-span-2 pt-5 row-span-2 font-light flex flex-col gap-2 col-start-3 row-start-3">
+        <div className="footer-partnerships">
           <h1 className="font-black">PARCERIAS</h1>
           <p>Quero ser parceiro</p>
         </div>
-        <div className="flex flex-wrap justify-around justify-self-center self-center col-start-3 row-start-5 col-span-2 uppercase font-black">
-            <h1 className={inter.className+" w-full text-center"}>Social</h1>
+        <div className="footer-social">
+            <h1 className={`footer-heading ${inter.className}`}>Social</h1>
             <Social src={icons.facebook}></Social>
             <Social src={icons.instagram}></Social>
             <Social src={icons.twitterx}></Social>
         </div>
-        <div className="row-start-6 col-span-4 col-start-1 font-medium tracking-wider justify-start self-end text-xs text-white">
-          <div className="flex gap-3 mb-3">
+        <div className="row-start-6 col-span-4 col-start-1 font-medium tracking-wider justify-start self-end text-xs text-white landscape:lg:col-span-4 landscape:lg:row-start-4">
+          <div className="flex flex-wrap gap-3 mb-3">
             <a>Política de Privacidade</a>
-            <a className="flex-1">Termos de Uso</a>
-          </div>
-          <div className="flex gap-3">
+            <a className="flex-1 md:flex-none min-w-max">Termos de Uso</a>
             <a>Cookies</a>
             <a className="flex-1">Acessibilidade</a>
             <p className="tracking-wider font-medium text-xs text-white">© {currentYear} {packageJson.org}</p>

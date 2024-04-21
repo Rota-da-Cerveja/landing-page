3a4f2ae77a51613072252b9a9f3da144e7c9b193
Silvio Cavalcanti <silviocavalcanti2011@gmail.com>
2024-04-21 01:08:08 -0300

    feat: finalizando a componentização e a responsividade

--- src/app/component/footer/footer.tsx
+++ src/app/component/footer/footer.tsx
@@ -1,55 +1,54 @@
 import Image from "next/image";
-import logo from "../../../../public/logo.webp"
 import { Inter } from "next/font/google";
 import { StoreButton } from "../button/store/Store";
 import { Social } from "../button/social/Social";
 import path from "path";
 import fs from 'fs';
-import { icons } from "../assets/icon";
 import './footer.css';
+import { icon, image } from "../assets/assets";
 
 const inter = Inter({subsets: ["latin"]});
 
 export function Footer() {
 
-    const packageJsonPath = path.join(process.cwd(), 'package.json');
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+  const packageJsonPath = path.join(process.cwd(), 'package.json');
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
 
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
   return (<footer className="footer-wrapper">
     <section className="footer-container">
-          <Image src={logo} alt="background" className="footer-logo"></Image>
-        <div className="footer-content">
-              <h1 className={`footer-heading ${inter.className}`}>em breve</h1>
-            <StoreButton title="App Store" icon={icons.appStore}></StoreButton>
-            <StoreButton title="Google Play" icon={icons.playStore}></StoreButton>
-        </div>
-        <div className="footer-links">
-          <a>Sobre</a>    
-          <a>Suporte</a>
-          <a>Fornecedores</a>
-        </div>
+      <Image src={image.logo} alt="background" className="footer-logo"></Image>
+      <div className="footer-content">
+        <h1 className={`footer-heading-center ${inter.className}`}>em breve</h1>
+        <StoreButton title="App Store" icon={icon.appStore}></StoreButton>
+        <StoreButton title="Google Play" icon={icon.playStore}></StoreButton>
+      </div>
+      <div className="footer-links">
+        <a href="#">Sobre</a>    
+        <a href="#">Suporte</a>
+        <a href="#">Fornecedores</a>
+      </div>
 
-        <div className="footer-partnerships">
-          <h1 className="font-black">PARCERIAS</h1>
-          <p>Quero ser parceiro</p>
+      <div className="footer-partnerships">
+        <h1 className="footer-heading">PARCERIAS</h1>
+        <p>Quero ser parceiro</p>
+      </div>
+      <div className="footer-social">
+        <h1 className={`footer-heading-center ${inter.className}`}>Social</h1>
+        <Social src={icon.facebook}></Social>
+        <Social src={icon.instagram}></Social>
+        <Social src={icon.twitterx}></Social>
+      </div>
+      <div className="footer-info">
+        <div className="footer-info-links">
+          <a href="#">Política de Privacidade</a>
+          <a href="#">Termos de Uso</a>
+          <a href="#">Cookies</a>
+          <a href="#">Acessibilidade</a>
+          <p>© {currentYear} {packageJson.org}</p>
         </div>
-        <div className="footer-social">
-            <h1 className={`footer-heading ${inter.className}`}>Social</h1>
-            <Social src={icons.facebook}></Social>
-            <Social src={icons.instagram}></Social>
-            <Social src={icons.twitterx}></Social>
-        </div>
-        <div className="row-start-6 col-span-4 col-start-1 font-medium tracking-wider justify-start self-end text-xs text-white landscape:lg:col-span-4 landscape:lg:row-start-4">
-          <div className="flex flex-wrap gap-3 mb-3">
-            <a>Política de Privacidade</a>
-            <a className="flex-1 md:flex-none min-w-max">Termos de Uso</a>
-            <a>Cookies</a>
-            <a className="flex-1">Acessibilidade</a>
-            <p className="tracking-wider font-medium text-xs text-white">© {currentYear} {packageJson.org}</p>
-          </div>
-        </div>
+      </div>
     </section>
   </footer>);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,10 @@
-import Image from "next/image";
 import { Footer } from "./component/footer/footer";
-import { image } from "./component/assets/image";
+import Header from "./component/header/Header";
 
 export default function Home() {
   return (
     <>
-      <main
-        className="flex bg-black bg-cover bg-left-top justify-center h-screen w-full landscape:lg:w-full bg-no-repeat text-white" style={{ backgroundImage: "url('" + image.background.src + "')" }}>
-           <div className="container landscape:lg:w-[calc(100vw-2.5rem)] p-4 lg:px-10 md:pt-20 lg:pt-8 pt-14 h-5/6 landscape:lg:h-full flex flex-col justify-between landscape:lg:justify-around">
-            <h2 className="text-xl md:text-3xl z-10 relative">
-              As melhores cervejas artesanais da sua região?
-            </h2>
-            <Image src={image.logo} alt="Logo" className="w-60 md:w-[60%] landscape:lg:w-[40%] z-10 relative"></Image>
-            <h3 className="w-64 text-base md:text-2xl md:w-[65%] font-bold z-10 relative">
-              O aplicativo desenvolvido para você ter a melhor experiência cervejeira possível.
-            </h3>
-            <div className="flex justify-start md:justify-center w-64 md:w-[65%] lg:justify-center">
-              <button className="bg-primary-700 py-4 md:py-8 px-7 w-full lg:w-[75%] lg:py-4 landscape:lg:w-max md:px-14 text-base lg:text-base md:text-3xl rounded z-10 relative">Quero Conhecer a Rota</button>
-            </div>
-          </div> 
-          <div className="absolute bg-cover lg:bg-contain bg-no-repeat h-[calc(100vh-3.5rem)] md:h-[calc(100vh-5rem)] right-0 aspect-half-glass landscape:lg:aspect-glass landscape:lg:right-[10%] -bottom-2 z-0"
-            style={{ backgroundImage: "url('" + image.glass.src + "')" }}
-          ></div>
-      </main>
-
+      <Header></Header>
       <Footer></Footer>
     </>
   );

cffe7d1ef22f576335f523663be29fe78c60055e
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2024-06-16 18:35:44 -0300

    feat: finalizado a pagina da cervejaria

--- src/app/component/recommendation/beerCard/beerCard.tsx
+++ src/app/component/recommendation/beerCard/beerCard.tsx
@@ -7,12 +7,12 @@ export type BeerProps = {
   description: string;
 };
 
-export const BeerCard: React.FunctionComponent<BeerProps> = ( props : BeerProps) => {
-  return <div className="max-w-md mx-auto mt-8">
-    <div className="beer-card">
-      <img src={props.src || "https://via.placeholder.com/300x200"} alt="Cerveja Artesanal" className="beer-image"/>
-        <h2 className="beer-name mt-2">{props.name}</h2>
-        <p className="beer-description">{props.description}</p>
-    </div>
+export const BeerCard: React.FunctionComponent<BeerProps> = (props: BeerProps) => {
+  return  <div className="beer-card">
+  <div className="beer-card-image-contanier">
+    <img src={props.src || "https://via.placeholder.com/300x200"} alt="Cerveja Artesanal" className="beer-image" />
   </div>
+  <h2 className="beer-name mt-2">{props.name}</h2>
+  <p className="beer-description">{props.description}</p>
+</div>
 }
